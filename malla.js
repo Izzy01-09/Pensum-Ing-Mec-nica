09e865bf0c22552017fe0dbbc5396750b1f2c13f
Izzy01-09 <isabellazu0109@gmail.com>
2025-07-25 21:24:46 -0500

    Create malla.js

---
+++ malla.js
@@ -0,0 +1,19 @@
+// Lista de materias (esto es solo un ejemplo con 3 materias)
+const materias = [
+  {
+    codigo: "452001",
+    nombre: "Cálculo I",
+    prerrequisitos: []
+  },
+  {
+    codigo: "452002",
+    nombre: "Cálculo II",
+    prerrequisitos: ["452001"]
+  },
+  {
+    codigo: "452003",
+    nombre: "Cálculo III",
+    prerrequisitos: ["452002"]
+  },
+  // Agregar aquí las demás materias del pensum
+];
